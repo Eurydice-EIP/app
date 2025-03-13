569f6d95878676fb683f2d21379706319792a09d
GitHub Action <action@github.com>
2025-03-13 13:20:32 +0000

    Squashed 'frontend/' changes from bbbaae0..f30158f
    f30158f Merge pull request #2 from Eurydice-EIP/feat/atomic-design
    25119fa feat(atomic-design): add basic components for atomic design
    e6389da feat(atomic-design): add basic components not working well
    
    git-subtree-dir: frontend
    git-subtree-split: f30158f516a469616f73edcb8ee24ea34294a70d

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+type ButtonProps = {
+  children?: React.ReactNode;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
+  className?: string;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  type = "button",
+  icon = null,
+  iconPosition = "left",
+  className = "",
+  ...props
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`flex items-center justify-center ${className}`}
+      {...props}
+    >
+      {icon && iconPosition === "left" && <span className="icon">{icon}</span>}
+      {children}
+      {icon && iconPosition === "right" && <span className="icon">{icon}</span>}
+    </button>
+  );
+};
+
+export default Button;
