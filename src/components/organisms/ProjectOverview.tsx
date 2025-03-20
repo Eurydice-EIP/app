955636b311372ee15458ae9a1b0ce2f51d41926f
GitHub Action <action@github.com>
2025-03-20 10:57:29 +0000

    Squashed 'frontend/' changes from e62dd07..da51316
    da51316 fix(main): Linter errors
    da8d29c Merge pull request #11 from Eurydice-EIP/feat/project-overview
    615e108 feat(project-overview): Added Advancement
    4c52b07 feat(project-overview): Added Done recently
    
    git-subtree-dir: frontend
    git-subtree-split: da51316cb8641fc024fba17c08b84cfd923f33ba

--- src/components/organisms/ProjectOverview.tsx
+++ src/components/organisms/ProjectOverview.tsx
@@ -1,5 +1,7 @@
 import ProgressBar from "../molecules/ProgressBar";
 import UpcomingTasks from "../molecules/UpcomingTasks";
+import DoneRecently from "../molecules/DoneRecently";
+import Advancement from "../molecules/Advancement";
 
 const ProjectOverview = () => {
   return (
@@ -14,9 +16,26 @@ const ProjectOverview = () => {
         max_value={100}
       ></ProgressBar>
       <hr className="my-6 border-1 rounded-xl border-[#B0E0E6]"></hr>
-      <div className="grid grid-cols-3 gap-10">
+      <div className="grid grid-cols-3 gap-5">
         <div className="col-span-2">
-          <UpcomingTasks className="my-2 rounded-xl px-4 py-2 bg-[#F4F7F8] border-[#B0E0E6] border-2"></UpcomingTasks>
+          <UpcomingTasks className="mt-2 rounded-xl px-4 py-2 bg-[#F4F7F8] border-[#B0E0E6] border-2"></UpcomingTasks>
+        </div>
+        <DoneRecently className="mt-2 rounded-xl px-4 py-2 bg-[#F4F7F8] border-[#B0E0E6] border-2"></DoneRecently>
+        <Advancement
+          className="mt-2 rounded-xl px-4 py-2 bg-[#F4F7F8] border-[#B0E0E6] border-2"
+          xp={248}
+          max_xp={2000}
+          money={28}
+          max_money={290}
+          streak={6}
+          tasks={8}
+          max_tasks={42}
+          days={5}
+        ></Advancement>
+        <div className="col-span-2 mt-2 rounded-xl px-4 py-2 bg-[#F4F7F8] border-[#B0E0E6] border-2 min-h-60">
+          <p className="text-2xl font-bold text-[#515351]">
+            Feature incoming...
+          </p>
         </div>
       </div>
     </div>
