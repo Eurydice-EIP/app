fd738b4dcf54819bc16f7bff6c28d66c47f15de0
GitHub Action <action@github.com>
2025-10-30 17:19:26 +0000

    Squashed 'backend/' changes from 3f36e35..a4f450c
    a4f450c Merge pull request #8 from Eurydice-EIP/chore/prisma
    ffa7c5f chore(prisma): implement PrismaService with module initialization
    d329cf1 chore(prisma): add initial Prisma schema configuration for PostgreSQL
    c1d341a chore(config): add environment configuration and validation for database and API
    
    git-subtree-dir: backend
    git-subtree-split: a4f450c260818c79668e8db4c41002e365257576

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/env.validation';
+import databaseConfig from './config/database.config';
+import apiConfig from './config/api.config';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot({
+        validate: validate,
+        load: [apiConfig, databaseConfig],
+        isGlobal: true,
+        cache: true,
+    })],
     controllers: [AppController],
     providers: [AppService],
 })
