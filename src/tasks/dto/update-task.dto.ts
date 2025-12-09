2e0e5bae075f0e23945b2ded21afe5d13728bbba
GitHub Action <action@github.com>
2025-12-09 00:52:48 +0000

    Squashed 'backend/' changes from 87a19fa..75d2928
    75d2928 fix(task): add status field to UpdateTaskDto and update method in TasksService
    
    git-subtree-dir: backend
    git-subtree-split: 75d292801437dbe5412afe6f38315aedea56e928

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateTaskDto } from './create-task.dto';
+import { TaskStatus } from '@prisma/client';
+import { IsEnum, IsOptional } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    @IsEnum(TaskStatus)
+    @IsOptional()
+    status?: TaskStatus;
+}
