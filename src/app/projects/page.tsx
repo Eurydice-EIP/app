052a44fbc64918aba788cf1f71d94db8a6dea996
GitHub Action <action@github.com>
2026-01-12 05:37:27 +0000

    Squashed 'frontend/' changes from 3a8d53b..2222560
    2222560 Merge pull request #32 from Eurydice-EIP/feat/login-page
    f465430 refactor(tasks): rename task fetching and creation functions for clarity
    4433ff5 feat(login): implement login form with email and password handling
    
    git-subtree-dir: frontend
    git-subtree-split: 222256024f984f9cd34b2cee44cb752cfe18054c

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,67 @@
+"use client";
+
+import { fetchTasks, createTask } from "@/api/Tasks";
+import React, { useEffect, useState } from "react";
+import { Tasks } from "@/types/Tasks";
+import Button from "@/components/atoms/Button";
+
 export default function Projects() {
-  return <div className="flex flex-row gap-10 p-8 text-black">Projects</div>;
+  const [tasks, setTasks] = useState<Tasks[] | null>(null);
+
+  useEffect(() => {
+    let mounted = true;
+    (async () => {
+      try {
+        const result = await fetchTasks();
+        if (!mounted) return;
+        setTasks(result);
+      } catch (err) {
+        console.error("Failed to load greeting:", err);
+        if (mounted) setTasks(null);
+      }
+    })();
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  const handleCreateTask = async () => {
+    const newTask: Tasks = {
+      title: "New Task",
+      dueAt: new Date().toISOString(),
+      userId: 1,
+      projectId: 1,
+      importance: 3,
+      estimatedTime: 4,
+    };
+    try {
+      const createdTask = await createTask(newTask);
+      setTasks((prevTasks) =>
+        prevTasks ? [...prevTasks, createdTask] : [createdTask]
+      );
+    } catch (err) {
+      console.error("Failed to create task:", err);
+    }
+  };
+
+  return (
+    <div className="flex flex-row gap-10 p-8 text-black">
+      Projects
+      <Button
+        className="border-[#B5B9BC] border-1 rounded-full"
+        onClick={handleCreateTask}
+      >
+        Create Task
+      </Button>
+      {tasks ? (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>{task.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading tasks...</p>
+      )}
+    </div>
+  );
 }
---
+++ src/types/Tasks.tsx
@@ -0,0 +1,9 @@
+export type Tasks = {
+  id?: number;
+  title: string;
+  dueAt: string;
+  userId: number;
+  projectId: number;
+  importance: number;
+  estimatedTime: number;
+};
