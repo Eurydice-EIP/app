134303aced0a6666290b4d9f498f5afbc6d8d425
GitHub Action <action@github.com>
2026-02-25 08:31:14 +0000

    Squashed 'frontend/' changes from b9a3c98..c98002c
    c98002c Merge pull request #40 from Eurydice-EIP/feat/project-creation
    1e4811a feat(project): add modal creation project
    
    git-subtree-dir: frontend
    git-subtree-split: c98002ca4b2ad24fc3d4e7781bbc2e451b519513

--- src/components/organisms/ProjectsBar.tsx
+++ src/components/organisms/ProjectsBar.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { useTranslations } from "next-intl";
 import Button from "../atoms/Button";
 import ProjectsSort from "../molecules/ProjectsSort";
 import { Project } from "@/types/Project";
 import ProjectCard from "../molecules/ProjectCard";
+import ProjectCreationModal from "./ProjectCreationModal";
 
 const sampleProjects: Project[] = [
   {
@@ -44,6 +46,7 @@ const sampleProjects: Project[] = [
 
 export default function ProjectsBar() {
   const t = useTranslations("projects");
+  const [isModalProjectOpen, setIsModalProjectOpen] = useState(false);
 
   return (
     <div className="flex flex-col border border-[var(--color-widget-border)] p-8 gap-4 rounded-[30px] items-center bg-[var(--color-widget-primary)] max-w-1/4 w-full">
@@ -51,7 +54,10 @@ export default function ProjectsBar() {
         <h2 className="text-[var(--color-text)] font-bold text-4xl">
           {t("title")}
         </h2>
-        <Button className="bg-[#EDDEA4] text-[#393E41] font-bold px-4 py-2 my-2 rounded-full hover:bg-[#E4D890] text-sm">
+        <Button
+          className="bg-[#EDDEA4] text-[#393E41] font-bold px-4 py-2 my-2 rounded-full hover:bg-[#E4D890] text-sm"
+          onClick={() => setIsModalProjectOpen(true)}
+        >
           {t("newProject")}
         </Button>
       </div>
@@ -59,6 +65,11 @@ export default function ProjectsBar() {
       {sampleProjects.map((project, index) => (
         <ProjectCard key={index} project={project} />
       ))}
+      {/* Modal */}
+      <ProjectCreationModal
+        isModalProjectOpen={isModalProjectOpen}
+        setIsModalProjectOpen={setIsModalProjectOpen}
+      />
     </div>
   );
 }
